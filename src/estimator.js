0cabea00cb773cc9ac0a84e0a35baf80c6b99602
phantware <phantmoney2011@gmail.com>
2020-04-11 09:47:47 +0100

    data input removed

--- src/estimator.js
+++ src/estimator.js
@@ -43,20 +43,4 @@ const covid19ImpactEstimator = (data) => {
   };
 };
 
-const input = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'dAYs',
-  timeToElapse: 28,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
-
-console.log(covid19ImpactEstimator(input));
-
 export default covid19ImpactEstimator;
