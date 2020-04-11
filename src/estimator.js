3dc2ce81f7396965b9774ad7035c18f981f7b5b8
phantware <phantmoney2011@gmail.com>
2020-04-11 09:46:25 +0100

    conditional statement for duration added

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,24 @@
-const computeInfectionsByRequestedTime = (currentlyInfected, days) => {
-  const factor = 2 ** ((1 / 3) * days);
-  return currentlyInfected * factor;
+const computeInfectionsByRequestedTime = (
+  currentlyInfected,
+  periodDuration,
+  pType
+) => {
+  let factor;
+  const periodType = pType.toLowerCase();
+  if (periodType === 'days') {
+    factor = 2 ** ((1 / 3) * periodDuration);
+  } else if (periodType === 'weeks') {
+    factor = 2 ** ((1 / 3) * periodDuration * 7);
+  } else if (periodType === 'months') {
+    factor = 2 ** ((1 / 3) * periodDuration * 30);
+  } else {
+    factor = 'unknown period';
+  }
+  return typeof factor === 'string' ? factor : currentlyInfected * factor;
 };
 
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases } = data;
+  const { reportedCases, periodType, timeToElapse } = data;
   const currentlyInfectedImpact = reportedCases * 10;
   const currentlyInfectedSevereImpact = reportedCases * 50;
 
@@ -14,17 +28,35 @@ const covid19ImpactEstimator = (data) => {
       currentlyInfected: currentlyInfectedImpact,
       infectionsByRequestedTime: computeInfectionsByRequestedTime(
         currentlyInfectedImpact,
-        7
+        timeToElapse,
+        periodType
       )
     },
     severeImpact: {
       currentlyInfected: currentlyInfectedSevereImpact,
       infectionsByRequestedTime: computeInfectionsByRequestedTime(
         currentlyInfectedSevereImpact,
-        7
+        timeToElapse,
+        periodType
       )
     }
   };
 };
+
+const input = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'dAYs',
+  timeToElapse: 28,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+
+console.log(covid19ImpactEstimator(input));
 
 export default covid19ImpactEstimator;
