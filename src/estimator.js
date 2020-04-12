c280f5bd6ff6f0c9c8ebc45eafa037e398a1a31e
phantware <phantmoney2011@gmail.com>
2020-04-12 19:34:14 +0100

    code rewrite adjusted

--- src/estimator.js
+++ src/estimator.js
@@ -10,12 +10,10 @@ const covid19ImpactEstimator = (data) => {
   } else if (data.periodType === 'month') {
     factor = Math.floor((30 * periodDuration) / 3);
   }
-  impact.currentlyInfacted = data.reportedCases * 10;
-  severeImpact.currentlyInfacted = data.reportedCases * 50;
-  impact.infectionByRequestedTime = impact.currentlyInfacted * 2 ** factor;
-  // eslint-disable-next-line operator-linebreak
-  severeImpact.infectionByRequestedTime =
-    severeImpact.currentlyInfacted * 2 ** factor;
+  impact.currentlyInfacted = ((data.reportedCases) * 10);
+  severeImpact.currentlyInfacted = ((data.reportedCases) * 50);
+  impact.infectionByRequestedTime = (impact.currentlyInfected * (2 ** factor));
+  severeImpact.infectionByRequestedTime = (severeImpact.currentlyInfected * (2 ** factor));
 
   const output = {
     data,
