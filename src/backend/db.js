f2e87f3747718a1d994acf9e288fb05ab4cad238
phantware <phantmoney2011@gmail.com>
2020-04-15 00:17:20 +0100

    console.log removed

--- src/backend/db.js
+++ src/backend/db.js
@@ -4,7 +4,7 @@ const connectionString = 'postgresql://postgres:postgres@localhost:5432/sdg_db';
 const pool = new Pool({ connectionString });
 
 pool.query('SELECT NOW()').then(({ rows: [time] }) => {
-  console.log('connected to database', time);
+  // console.log('connected to database', time);
 });
 
 export default pool;
