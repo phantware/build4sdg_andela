fb600c5701c12fcc2dc3c12cf9a421a8f60d4fff
phantware <phantmoney2011@gmail.com>
2020-04-15 00:30:03 +0100

    time disabled

--- src/backend/db.js
+++ src/backend/db.js
@@ -3,6 +3,7 @@ import { Pool } from 'pg';
 const connectionString = 'postgresql://postgres:postgres@localhost:5432/sdg_db';
 const pool = new Pool({ connectionString });
 
+// eslint-disable-next-line no-unused-vars
 pool.query('SELECT NOW()').then(({ rows: [time] }) => {
   // console.log('connected to database', time);
 });
