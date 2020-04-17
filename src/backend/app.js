20c21f51f16cecaf5efdd6357d5c8728b546fdaa
phantware <phantmoney2011@gmail.com>
2020-04-17 10:11:09 +0100

    welcome message updated

--- src/backend/app.js
+++ src/backend/app.js
@@ -10,7 +10,13 @@ app.use(express.json());
 app.use(patientRouter);
 
 app.get('/api/v1/on-covid-19', (req, res) => {
-  res.status(200).send({ message: 'Testing covid-19 endpoint' });
+  res.status(200).send({
+    message: 'BuildForSDG Cohort-1 JavaScript Assessment',
+    message2:
+      '<p>Too many patients, not enough hospitals and beds. A serious shortage of ventilators, masks and other PPE - if we don’t practice social distancing</p>',
+    message3:
+      '<p>Job losses or freezes, low cash flow and low production (even for essentials like food). These and more from too many people being sick, a sizable number dying (including some of the best people in many fields), and many others affected by the impact of losing loved ones or a world operating in slow motion </p>'
+  });
 });
 
 const port = process.env.PORT || 5050;
