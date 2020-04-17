bb2d7eb6c0e717407f468624da626d62e61505f1
phantware <phantmoney2011@gmail.com>
2020-04-17 08:41:56 +0100

    checking if input is json or xml to print different result

--- src/backend/controllers/infected.js
+++ src/backend/controllers/infected.js
@@ -39,25 +39,74 @@ class Data {
   }
 
   static async patientRecordLogs(req, res) {
-    const startTime = Date.now();
     const { type } = req.params;
-    if (type === 'json') res.status(201).json({ success: 'hello world' });
-    else if (type === 'xml') {
-      res.type('application/xml');
-      res.status(201).send(
-        xml({
-          data: 'data1',
-          status: '45'
-        })
+    if (type === 'json') {
+      const startTime = Date.now();
+      const {
+        periodType,
+        timeToElapse,
+        reportedCases,
+        population,
+        totalHospitalBeds
+      } = req.body;
+      const data = {
+        region: {
+          name: 'Africa',
+          avgAge: 19.7,
+          avgDailyIncomeInUSD: 5,
+          avgDailyIncomePopulation: 0.71
+        },
+        periodType,
+        timeToElapse,
+        reportedCases,
+        population,
+        totalHospitalBeds
+      };
+
+      const resData = estimator(data);
+
+      res.status(200).send(resData);
+      const endTime = Date.now();
+
+      const reqLog = [req.method, req.url, res.statusCode, endTime - startTime];
+      pool.query(
+        'insert into logs(method,url,status,log_time) values($1,$2,$3,$4) returning *',
+        reqLog
+      );
+    } else if (type === 'xml') {
+      const startTime = Date.now();
+      const {
+        periodType,
+        timeToElapse,
+        reportedCases,
+        population,
+        totalHospitalBeds
+      } = req.body;
+      const data = {
+        region: {
+          name: 'Africa',
+          avgAge: 19.7,
+          avgDailyIncomeInUSD: 5,
+          avgDailyIncomePopulation: 0.71
+        },
+        periodType,
+        timeToElapse,
+        reportedCases,
+        population,
+        totalHospitalBeds
+      };
+
+      const resData = estimator(data);
+
+      res.status(200).send(resData);
+      const endTime = Date.now();
+
+      const reqLog = [req.method, req.url, res.statusCode, endTime - startTime];
+      pool.query(
+        'insert into logs(method,url,status,log_time) values($1,$2,$3,$4) returning *',
+        reqLog
       );
     }
-    const endTime = Date.now();
-
-    const reqLog = [req.method, req.url, res.statusCode, endTime - startTime];
-    pool.query(
-      'insert into logs(method,url,status,log_time) values($1,$2,$3,$4) returning *',
-      reqLog
-    );
   }
 }
 
