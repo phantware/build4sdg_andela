527110343b84c0161c5c8a0d56f16b0c83d4f9f0
phantware <phantmoney2011@gmail.com>
2020-04-17 12:49:25 +0100

    middleware adjusted

--- src/backend/middleware/infected.js
+++ src/backend/middleware/infected.js
@@ -4,7 +4,7 @@ import Joi from '@hapi/joi';
 class PatientRecordValidation {
   static async patientRecord(req, res, next) {
     const schema = Joi.object({
-      periodType: Joi.string().min(5).max(5).trim().lowercase().required(),
+      periodType: Joi.string().min(5).max(3).trim().lowercase().required(),
       timeToElapse: Joi.number().min(1).required(),
       reportedCases: Joi.number().min(1).required(),
       population: Joi.number().min(1).required(),
@@ -21,7 +21,7 @@ class PatientRecordValidation {
 
   static async patientRecordLogs(req, res, next) {
     const schema = Joi.object({
-      periodType: Joi.string().min(5).max(5).trim().lowercase().required(),
+      periodType: Joi.string().min().max(3).trim().lowercase().required(),
       timeToElapse: Joi.number().min(1).required(),
       reportedCases: Joi.number().min(1).required(),
       population: Joi.number().min(1).required(),
